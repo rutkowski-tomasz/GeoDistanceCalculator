dd45e4a5512ac5e207e5b794418a0ec6d80cdd73
Tomasz Rutkowski <tomasz.rutkowski@inspeerity.com>
2022-01-29 20:35:12 +0100

    Map endpoint to use actual service

--- src/Application/Endpoints/GeoDistance/CalculateGeoDistanceEndpoint.cs
+++ src/Application/Endpoints/GeoDistance/CalculateGeoDistanceEndpoint.cs
@@ -1,15 +1,54 @@
-﻿using Ardalis.ApiEndpoints;
+﻿using Application.Common.Interfaces;
+using Ardalis.ApiEndpoints;
+using Domain.Entities;
+using Domain.ValueObjects;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Endpoints.GeoDistance;
 
+public class CalculateGeoDistanceRequest
+{
+    [FromBody]
+    public double LocationALatitude { get; set; }
+    
+    [FromBody]
+    public double LocationALongitude { get; set; }
+    
+    [FromBody]
+    public double LocationBLatitude { get; set; }
+    
+    [FromBody]
+    public double LocationBLongitude { get; set; }
+}
+
 public class CalculateGeoDistanceEndpoint : EndpointBaseAsync
-    .WithoutRequest
+    .WithRequest<CalculateGeoDistanceRequest>
     .WithResult<double>
 {
-    [HttpGet("calculate")]
-    public override async Task<double> HandleAsync(CancellationToken cancellationToken = new CancellationToken())
+    private readonly IGeoDistanceService _geoDistanceService;
+
+    public CalculateGeoDistanceEndpoint(IGeoDistanceService geoDistanceService)
     {
-        return await Task.FromResult(0d);
+        _geoDistanceService = geoDistanceService;
+    }
+
+    [HttpPost("geo-distance/calculate")]
+    public override async Task<double> HandleAsync(CalculateGeoDistanceRequest request, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var locationA = new GeoLocation
+        {
+            Latitude = Latitude.From(request.LocationALatitude),
+            Longitude = Longitude.From(request.LocationALongitude)
+        };
+        
+        var locationB = new GeoLocation
+        {
+            Latitude = Latitude.From(request.LocationBLatitude),
+            Longitude = Longitude.From(request.LocationBLongitude)
+        };
+        
+        var distance = await _geoDistanceService.CalculateDistanceAsync(locationA, locationB);
+
+        return distance;
     }
 }
