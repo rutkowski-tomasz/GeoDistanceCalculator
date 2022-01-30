98e5a5fc354dbaef37f9e1aacdfee9206d61f0d8
Tomasz Rutkowski <tomasz.rutkowski@inspeerity.com>
2022-01-30 17:40:00 +0100

    Create expectations tests for new calculation methods

---
+++ tests/Infrastructure.Tests.Unit/Services/GeoDistanceCalculation/ThreeDimensionPointDistanceCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.ValueObjects;
+using FluentAssertions;
+using Infrastructure.Services.GeoDistanceCalculation;
+using Xunit;
+
+namespace Infrastructure.Tests.Unit.Services.GeoDistanceCalculation;
+
+public class ThreeDimensionPointDistanceCalculatorTests : IClassFixture<ThreeDimensionPointDistanceCalculator>
+{
+    private readonly ThreeDimensionPointDistanceCalculator _distanceCalculator;
+
+    public ThreeDimensionPointDistanceCalculatorTests(ThreeDimensionPointDistanceCalculator distanceCalculator)
+    {
+        _distanceCalculator = distanceCalculator;
+    }
+    
+    [Fact]
+    public async Task ShouldCalculateApproximatelyValidDistance()
+    {
+        var locationA = new GeoLocation
+        {
+            Latitude = Latitude.From(53.297975),
+            Longitude = Longitude.From(-6.372663)
+        };
+
+        var locationB = new GeoLocation
+        {
+            Latitude = Latitude.From(41.385101),
+            Longitude = Longitude.From(-81.440440)
+        };
+
+        var distance = await _distanceCalculator.CalculateDistanceAsync(locationA, locationB);
+
+        distance.Value.Should().BeApproximately(5536, 100);
+    }
+}
