b859d685e34868134afb6ee9bf858c798e74b0d4
Tomasz Rutkowski <tomasz.rutkowski@inspeerity.com>
2022-01-31 19:37:32 +0100

    Add operation and validator tests

---
+++ tests/Application.Tests.Unit/Swagger/OperationSummaryTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NSwag;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+using Xunit;
+
+namespace Application.Tests.Unit.Swagger;
+
+public class OperationSummaryTests
+{
+    [Fact]
+    public void EachOperationProcessorShouldSetCustomSummary()
+    {
+        typeof(IApplicationMarker)
+            .Assembly
+            .GetExportedTypes()
+            .Where(p => p.IsAssignableTo(typeof(IOperationProcessor)))
+            .Select(Activator.CreateInstance)
+            .Cast<IOperationProcessor>()
+            .Select(ProcessAndCheckIfSummaryIsUpdated)
+            .Should()
+            .AllBeEquivalentTo(true);
+    }
+
+    private bool ProcessAndCheckIfSummaryIsUpdated(IOperationProcessor processor)
+    {
+        var context = CreateProcessorContext();
+        processor.Process(context);
+        return !string.IsNullOrEmpty(context.OperationDescription.Operation.Summary);
+    }
+
+    private OperationProcessorContext CreateProcessorContext()
+    {
+        return new OperationProcessorContext(
+            new OpenApiDocument(),
+            new OpenApiOperationDescription()
+            {
+                Operation = new OpenApiOperation()
+            },
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+    }
+}
